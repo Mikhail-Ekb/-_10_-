c981344a20045db97adeefe8d307782a798cb847
MIKHAIL <MIKHAIL@DESKTOP-6MU8OHN>
2022-11-27 23:20:27 +0500

    Добавьте файлы проекта.

---
+++ Задача_10_Работа над ошибками/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_10_Работа_над_ошибками
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Angle angle;
+            do
+            {
+                    Console.Write("Введите градусы: ");
+                    int gradus_ = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Введите минуты: ");
+                    int min_ = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Введите секунды: ");
+                    int sec_ = Convert.ToInt32(Console.ReadLine());
+
+                    angle = new Angle(gradus_, min_, sec_);
+
+                if (!angle.isCorrect)
+                    Console.WriteLine("\nОшибка. Повторите ввод.");
+
+            } while (angle.isCorrect == false);
+
+            double radian = angle.ToRadians();
+            Console.Write($"Радианы: {radian}");
+            Console.ReadLine();
+        }
+    }
+}
